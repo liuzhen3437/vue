cb6c34c707cc04e02295c6a1061328a2dfe28f48
LZ <abc983955071@qq.com>
2020-05-07 15:19:59 +0800

    更正

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // https://github.com/michael-ciniawsky/postcss-load-config
 
 module.exports = {
@@ -9,15 +8,3 @@ module.exports = {
     "autoprefixer": {}
   }
 }
-=======
-// https://github.com/michael-ciniawsky/postcss-load-config
-
-module.exports = {
-  "plugins": {
-    "postcss-import": {},
-    "postcss-url": {},
-    // to edit target browsers: use "browserslist" field in package.json
-    "autoprefixer": {}
-  }
-}
->>>>>>> 4c0d6bcf0e22fb516f0fee4f8d27a3508f239063
